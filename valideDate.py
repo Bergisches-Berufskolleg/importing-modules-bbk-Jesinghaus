3ccf4f6ea01dc654494792c72707b4e96aca918b
Ben <24-hbfi1.jesinghaus@bbk.schule>
2025-05-08 08:50:41 +0200

    nr 13

--- valideDate.py
+++ valideDate.py
@@ -1,7 +1,38 @@
 from leapYear import isLeapYear
 
+from leapYear import isLeapYear
+
 def validateDate(day, month, year):
-    # ...
+    if month < 1 or month > 12:
+        return False
+    if day < 1 or day > 31:
+        return False
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        if day > 31:
+            return False
+        else:
+            return True
+    if month == 2:
+        if isLeapYear(year):
+            if day == 28:
+                return True
+            if day > 29:
+                return False
+        else:
+            if day > 28:
+                return False
+    if month == (1, 3, 5, 7, 8, 10, 12) and day > 31:
+        return False
+    if month == 9 and day > 30:
+        return False
+    if month == 11 and day > 30:
+        return False
+    if month == 4 and day > 30:
+        return False
+    if month == 6 and day > 30:
+        return False
+    if year < 0:
+        return False
     return True
 
 def main():
@@ -10,5 +41,5 @@ def main():
     year  = int(input("Gib ein Jahr ein: "))
     print(validateDate(day, month, year))
 
-if __name__ == "__main__":
+if __name__ == "main":
     main()
